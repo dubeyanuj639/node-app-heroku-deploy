7dde6adde6fb8903e44bc20c03b40a7687cf659e
anujdubey <dubeyanuj639@gmail.com>
2022-03-30 17:28:53 +0530

    First Commit.

---
+++ main.js
@@ -0,0 +1,13 @@
+// ESM syntax is supported.
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/', (req, res) => {
+  return res.status(200).send("Testing Node.js Deployment on Heroku....")
+})
