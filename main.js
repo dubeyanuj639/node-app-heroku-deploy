5b8eb92a3f3412528c4d0f6a76c5aa532b1a5faf
anujdubey <dubeyanuj639@gmail.com>
2022-03-30 17:57:00 +0530

    changed port.

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(process.env.port, () => {
+  console.log(`Example app listening on port ${process.env.port}`)
 })
 
 app.get('/test', (req, res) => {
